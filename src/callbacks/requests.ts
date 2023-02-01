54fe9eff8298a5b86600e3c46bc6a0752a937084
Danielle-Luz <daniln2209@gmail.com>
2023-02-01 02:10:44 -0300

    feat: implements create movie function

--- src/callbacks/requests.ts
+++ src/callbacks/requests.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
+import { database } from "../database";
+import { iMessage } from "../interfaces";
 
 export namespace requests {
   export const createMovie = (request: Request, response: Response) => {
-    const { body } = request;
+    const { body: newMovie } = request;
+    const sucessMessage: iMessage = { message: "Filme inserido com sucesso." };
+
+    database.createMovie(newMovie);
+
+    response.status(201).send(sucessMessage);
   };
 }

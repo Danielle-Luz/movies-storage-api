05d6cb741a165f5e13a23526e0eed7d19940edb3
Danielle-Luz <daniln2209@gmail.com>
2023-02-01 01:06:50 -0300

    feat: add post route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import { middlewares } from "./callbacks/middlewares";
+import { requests } from "./callbacks/requests";
+
 const express = require("express");
 const api = express();
 
 api.use(express.json());
+
+api.post(
+  "/movies",
+  middlewares.checkMovieKeys,
+  middlewares.checkIfNameAlreadyExists,
+  middlewares.checkMoviePropertiesTypes,
+  requests.createMovie
+);
 
 api.listen(3000, () => console.log("API is running :))"));

42e85f16e38c72ccd2109a82ba0377246821b265
Danielle-Luz <daniln2209@gmail.com>
2023-02-01 19:19:25 -0300

    feat: remove not used error message

--- src/callbacks/requests.ts
+++ src/callbacks/requests.ts
@@ -4,24 +4,16 @@ import { iMessage } from "../interfaces";
 
 export namespace requests {
   export const createMovie = async (request: Request, response: Response) => {
-    let status: number;
-    let message: iMessage;
 
     try {
       const { body: newMovie } = request;
-
-      status = 201;
-      message = {
-        message: "Filme inserido com sucesso.",
-      };
 
       const createdMovie = await database.createMovie(newMovie);
 
       return response.status(201).json(createdMovie);
     } catch (error) {
-      status = 500;
-      message = { message: "Erro ao processar a solicitação na base de dados" };
+      const message: iMessage = { message: "Erro ao processar a solicitação na base de dados" };
+      response.status(500).send(message);
     }
-    response.status(status).send(message);
   };
 }
